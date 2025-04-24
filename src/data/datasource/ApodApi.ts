6e8a2d3ee9a4048777aed17b29acfe829754c961
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-23 22:28:51 -0600

    Actualizaciones de comentarios y doc

--- src/data/datasource/ApodApi.ts
+++ src/data/datasource/ApodApi.ts
@@ -5,6 +5,10 @@ import { ApodNasaModel } from "../models/ApodNasa";
 const { BASE_URL, API_KEY, TIMEOUT } = Api_Configuration;
 
 export class ApodApi {
+    /**
+     * Funcion para hacer una peticion a la api de la nasa de APOD
+     * @returns ApodNasaModel
+     */
     async fechtDataApod(): Promise<ApodNasaModel> {
         const res = await axios.get(
             `${BASE_URL}/planetary/apod?api_key=${API_KEY}`,
@@ -16,6 +20,11 @@ export class ApodApi {
         return res.data;
     }
 
+    /**
+     * Funcion para hacer una peticion a la api de la nasa de APOD en base a una fecha
+     * @param formattedDate - Fecha en formato YYYY-MM-DD
+     * @returns ApodNasaModel
+     */
     async fechtDataOtherDateApod(formattedDate: string): Promise<ApodNasaModel> {
         const res = await axios.get(
             `${BASE_URL}/planetary/apod?date=${formattedDate}&api_key=${API_KEY}`,
