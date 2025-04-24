2a25b9acc24d1a1f00b861a6bb599a687a209f8f
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-24 15:31:39 -0600

    Update NeoWsMapper.ts
    Actualización del mapper para que recorra el arreglo de objetos dentro de photos

--- src/data/mappers/NeoWsMapper.ts
+++ src/data/mappers/NeoWsMapper.ts
@@ -1,23 +1,33 @@
 import { NeoWs } from "@/domain/entities/NeoWs";
-import { NearEarthObject, NeoWsNasaModel } from "../models/NeoWsNasa";
+import { NeoWsNasaModel } from "../models/NeoWsNasa";
 
-export const neoWsMapper = (model: NearEarthObject): NeoWs => ({
-    id: model.id,
-    name: model.name,
-    orbitingBody: model.close_approach_data[0].orbiting_body ?? 'Unkonw',
-    absoluteMagnitude: model.absolute_magnitude_h,
-    estimatedDiameterKilometers: {
-        max: model.estimated_diameter.kilometers.estimated_diameter_max,
-        min: model.estimated_diameter.kilometers.estimated_diameter_min,
-    },
-    estimatedDiameterMeters: {
-        max: model.estimated_diameter.meters.estimated_diameter_max,
-        min: model.estimated_diameter.meters.estimated_diameter_min,
-    },
-    isPotentiallyHazardous: model.is_potentially_hazardous_asteroid,
-    closeApproachData: {
-        date: model.close_approach_data[0].close_approach_date_full,
-        relativeVelocityKps: model.close_approach_data[0].relative_velocity.kilometers_per_second,
-        missDistanceKm: model.close_approach_data[0].miss_distance.kilometers
-    }
-});
+export const neoWsMapper = (model: NeoWsNasaModel): NeoWs[] =>
+    model.near_earth_objects.map(obj => {
+        const approach = obj.close_approach_data[0];
+
+        return {
+            id: obj.id,
+            name: obj.name,
+            orbitingBody: approach?.orbiting_body ?? 'Unknown',
+            absoluteMagnitude: obj.absolute_magnitude_h,
+            estimatedDiameterKilometers: {
+                max: obj.estimated_diameter.kilometers.estimated_diameter_max,
+                min: obj.estimated_diameter.kilometers.estimated_diameter_min,
+            },
+            estimatedDiameterMeters: {
+                max: obj.estimated_diameter.meters.estimated_diameter_max,
+                min: obj.estimated_diameter.meters.estimated_diameter_min,
+            },
+            isPotentiallyHazardous: obj.is_potentially_hazardous_asteroid,
+            closeApproachData: approach ? {
+                date: approach.close_approach_date_full,
+                relativeVelocityKps: approach.relative_velocity.kilometers_per_second,
+                missDistanceKm: approach.miss_distance.kilometers,
+            } : 
+            {
+                date: 'Unknown',
+                relativeVelocityKps: 0,
+                missDistanceKm: 0,
+            },
+        };
+    });
