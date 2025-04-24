b02f82c4c2ceed59a5e12fc9b615a9db4c94309e
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-24 00:27:10 -0600

    Create NeoWsApi.ts
    Medios de la api de neows

---
+++ src/data/datasource/NeoWsApi.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { Api_Configuration } from "@/core/api";
+import { NeoWsNasaModel } from "../models/NeoWsNasa";
+
+
+const { BASE_URL, API_KEY, TIMEOUT } = Api_Configuration;
+
+export class NeoWsApi {
+    async fechtDataNeoWs(page: number, size: number = 20): Promise<NeoWsNasaModel[]> {
+        const res = await axios.get(
+            `${BASE_URL}/neo/rest/v1/neo/browse?page=${page}&size=${size}&api_key=${API_KEY}`,
+            {
+                timeout: TIMEOUT
+            }
+        );
+
+        return res.data;
+    }
+
+    async fechtDataForIdNeoWs(id: string): Promise<NeoWsNasaModel> {
+        const res = await axios.get(
+            `${BASE_URL}/neo/rest/v1/neo/${id}?api_key=${API_KEY}`,
+            {
+                timeout: TIMEOUT
+            }
+        );
+
+        return res.data;
+    }
+}
