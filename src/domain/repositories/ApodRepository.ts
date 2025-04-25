a0dabe459d606dc21d84bbdc72730227cdb50b13
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-24 21:57:30 -0600

    Update ApodRepository.ts
    Agregamos mas medios para manejar los datos de manera offline y poder hacer uso de asyncstorage

--- src/domain/repositories/ApodRepository.ts
+++ src/domain/repositories/ApodRepository.ts
@@ -2,6 +2,8 @@ import { Apod } from "../entities/Apod";
 
 export interface ApodRepository {
     fetchDataApod(): Promise<Apod>;
-    fetchDataApodOffLine(): Promise<Apod>;
-    fetchDataOtherDateApod(date: Date): Promise<Apod>
+    fetchDataOtherDateApod(date: Date): Promise<Apod | null>
+    fetchDataApodOffLine(): Promise<Apod | null>;
+    saveData(apod: Apod): Promise<void>;
+    deleteData(): Promise<void>;
 }
