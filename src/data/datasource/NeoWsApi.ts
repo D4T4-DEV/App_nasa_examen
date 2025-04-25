a2d18948b11bedb17a486d3c383e004088177bd4
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-24 23:25:38 -0600

    Update NeoWsApi.ts
    Cambio para la correcta devolucion del dato

--- src/data/datasource/NeoWsApi.ts
+++ src/data/datasource/NeoWsApi.ts
@@ -6,7 +6,7 @@ import { NeoWsNasaModel } from "../models/NeoWsNasa";
 const { BASE_URL, API_KEY, TIMEOUT } = Api_Configuration;
 
 export class NeoWsApi {
-    async fetchDataNeoWs(page: number, size: number = 20): Promise<NeoWsNasaModel[]> {
+    async fetchDataNeoWs(page: number, size: number = 20): Promise<NeoWsNasaModel> {
         const res = await axios.get(
             `${BASE_URL}/neo/rest/v1/neo/browse?page=${page}&size=${size}&api_key=${API_KEY}`,
             {
