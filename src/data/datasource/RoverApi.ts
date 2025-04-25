2f31dc270f5b5d1f9089b0bcf775b98f1896c715
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-25 09:07:34 -0600

    Update RoverApi.ts
    Actualizamos que valor devuelve

--- src/data/datasource/RoverApi.ts
+++ src/data/datasource/RoverApi.ts
@@ -6,7 +6,7 @@ const { BASE_URL, API_KEY, TIMEOUT } = Api_Configuration;
 
 export class RoverApi {
 
-    async fetchDataRover(sunValue: number = 1000, page: number = 0): Promise<RoverNasaModel[]> {
+    async fetchDataRover(sunValue: number = 1000, page: number = 0): Promise<RoverNasaModel> {
         const res = await axios.get(
             `${BASE_URL}/mars-photos/api/v1/rovers/curiosity/photos?sol=${sunValue}&page=${page}&api_key=${API_KEY}`,
             { timeout: TIMEOUT }
