2c59b73d761d6d925da206204a89fed674b33e18
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-26 18:23:33 -0600

    Update RootNavigator.tsx
    Actualizacion de tipos en el nombre dado el cambio

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -9,9 +9,9 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export default function RootNavigator() {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="Home" component={HomeScreen} />
-            <Stack.Screen name="APOD" component={TabsStack} options={{ title: 'Explorando APOD' }} />
-            <Stack.Screen name="EPIC" component={DrawerStack} options={{ headerShown: false }} />
+            <Stack.Screen name="Home" component={HomeScreen} options={{ title: 'NASA APP' }} />
+            <Stack.Screen name="Apod" component={TabsStack} options={{ title: 'Explorando APOD' }} />
+            <Stack.Screen name="Epic" component={DrawerStack} options={{ headerShown: false }} />
         </Stack.Navigator>
     );
 }
