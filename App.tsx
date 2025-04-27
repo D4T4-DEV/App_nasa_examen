75d930ead4b6673b13cc98096df139e5adf004b3
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-26 18:22:00 -0600

    Update App.tsx
    Se cambia y se implementa "GestureHandlerRootView " dado un error al generar tarjetas con react-native-paper

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
-import { linking } from '@/navigation/linking';
+import React from 'react';
 import RootNavigator from '@/navigation/RootNavigator';
+import { StyleSheet } from 'react-native';
+import { linking } from '@/navigation/linking';
 import { useTheme } from '@/presentation/hooks/useTheme';
 import { store } from '@/store/store';
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { PaperProvider } from 'react-native-paper';
 import { Provider } from 'react-redux';
-
 
 function AppContent() {
   const {
@@ -19,7 +21,7 @@ function AppContent() {
     <PaperProvider theme={paperTheme}>
       <StatusBar style={isDarkContext ? 'dark' : 'light'} />
       <NavigationContainer linking={linking} theme={navigationTheme}>
-        <RootNavigator />:
+        <RootNavigator />
       </NavigationContainer>
     </PaperProvider>
   );
@@ -28,7 +30,15 @@ function AppContent() {
 export default function App() {
   return (
     <Provider store={store}>
-      <AppContent />
+      <GestureHandlerRootView style={styles.container}>
+        <AppContent />
+      </GestureHandlerRootView>
     </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
