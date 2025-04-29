f3fcf6917c390fad5075726a1d9595901e7694bd
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-28 18:20:48 -0600

    Update SearchImageScreen.tsx
    Fallback cuando no tiene red

--- src/presentation/screens/earth/SearchImageScreen.tsx
+++ src/presentation/screens/earth/SearchImageScreen.tsx
@@ -4,29 +4,47 @@ import { FormularioEarth } from '@/presentation/components/earth/formEarth'
 import { ScrollView } from 'react-native-gesture-handler'
 import { useEarthViewModel } from '@/presentation/viewmodels/useEarthViewModel'
 import { ImageRenderEarth } from '@/presentation/components/earth/ImgRenderEarth'
+import { useConnectivity } from '@/presentation/hooks/useConnectivity'
+import NoWifiSvg from '@/presentation/components/svgs/NoWifiSvg'
 
 const SearchImageScreen = () => {
     const { earthData } = useEarthViewModel();
+    const { isConnected } = useConnectivity();
 
     return (
-        <ScrollView style={styles.container}>
-            <FormularioEarth />
-            <View style={styles.containerImg}>
-                <ImageRenderEarth
-                    loading={earthData.loading}
-                    error={earthData.error}
-                    data={earthData.data}
-                    isNecesary={true}
-                />
-            </View>
-        </ScrollView>
+        <View style={isConnected ? { flex: 1 } : styles.container}>
+            {isConnected ? (
+                <ScrollView style={styles.containerScroll}>
+                    <FormularioEarth />
+                    <View style={styles.containerImg}>
+                        <ImageRenderEarth
+                            loading={earthData.loading}
+                            error={earthData.error}
+                            data={earthData.data}
+                            isNecesary={true}
+                        />
+                    </View>
+                </ScrollView>
+            ) : (
+                <View style={styles.containerImg}>
+                    <NoWifiSvg description="No tienes internet para usar este recurso" />
+                </View>
+            )}
+        </View>
     )
+
 }
 
 export default SearchImageScreen
 
 const styles = StyleSheet.create({
     container: {
+        flex: 1,
+        alignItems: 'center',
+        alignContent: 'center',
+        justifyContent: 'center',
+    },
+    containerScroll: {
         flex: 1,
         gap: 20
     },
