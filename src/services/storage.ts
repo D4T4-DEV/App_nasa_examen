ac0a267b530f4a98d8906c1bf079c815cb787521
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-04-30 21:35:42 -0600

    Update storage.ts
    Fallback para web, para poder guardar datos de manera correcta cuando se tenga la aplicacion corriendo en web

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,17 +1,45 @@
+import { usePlataform } from "@/presentation/hooks/usePlataform";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const { isWeb } = usePlataform();
 
 export const LocalStorage = {
 
     async save<T>(key: string, value: T): Promise<void> {
-        await AsyncStorage.setItem(key, JSON.stringify(value));
+        const jsonValue = JSON.stringify(value);
+        if (isWeb) {
+            localStorage.setItem(key, jsonValue);
+        } else {
+            await AsyncStorage.setItem(key, jsonValue);
+        }
     },
 
     async load<T>(key: string): Promise<T | null> {
-        const result = await AsyncStorage.getItem(key);
-        return result ? (JSON.parse(result) as T) : null;
+
+        if (isWeb) {
+            const result = localStorage.getItem(key);
+            return result ? (JSON.parse(result) as T) : null;
+        } else {
+            const result = await AsyncStorage.getItem(key);
+            return result ? (JSON.parse(result) as T) : null;
+        }
+
     },
 
     async remove(key: string): Promise<void> {
-        await AsyncStorage.removeItem(key);
+        if (isWeb) {
+            localStorage.removeItem(key);
+        } else {
+            await AsyncStorage.removeItem(key);
+        }
+
     },
+
+    async clearAll(): Promise<void> {
+        if (isWeb) {
+            localStorage.clear();
+        } else {
+            await AsyncStorage.clear();
+        }
+    }
 };
