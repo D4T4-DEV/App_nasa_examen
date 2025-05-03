9f83f9c43810adc3fb230cd17c73547d121d78b1
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-05-03 08:14:40 -0600

    Update neowsSlice.ts
    Actualizacion para que maneje la pagina y el status si tiene mas paginas, considerando que no se repitan los datos cuando se devuelve un id igual

--- src/store/slices/neowsSlice.ts
+++ src/store/slices/neowsSlice.ts
@@ -5,16 +5,20 @@ import { createSlice } from "@reduxjs/toolkit";
 import { deleteNeows, fetchNeows, fetchNeowsForId, fetchNeowsOffline, saveNeows } from "../thunks/neowsThunk";
 
 interface NeowsState {
-    fetchData: RequestState<NeoWs[]>;
+    fetchData: RequestState<NeoWs[]>;  // Datos de NeoWs
     fetchDataForId: RequestState<NeoWs>;
     fetchDataOffline: RequestState<NeoWs[]>;
+    page: number;  // Página actual
+    noMorePages: boolean;  // Indica si no hay más páginas
 }
 
-// Estado incial
+// Estado inicial
 const initialState: NeowsState = {
     fetchData: createRequestState<NeoWs[]>(),
     fetchDataForId: createRequestState<NeoWs>(),
     fetchDataOffline: createRequestState<NeoWs[]>(),
+    page: 0,
+    noMorePages: false,
 }
 
 // Slice
@@ -22,28 +26,61 @@ export const neowsSlice = createSlice({
     name: 'neows',
     initialState,
     reducers: {
-        // Borrar los estados del slice
+        // Limpiar los estados del slice
         clearNeows(state) {
             state.fetchData = { ...initialState.fetchData };
             state.fetchDataForId = { ...initialState.fetchDataForId };
             state.fetchDataOffline = { ...initialState.fetchDataOffline };
+            state.page = 0;
+            state.noMorePages = false;
         },
+        // Establecer si ya no hay mas paginas
+        setNoMorePages(state, action) {
+            state.noMorePages = action.payload;
+        },
+        // Establecer la pagina actual
+        setPage(state, action) {
+            state.page = action.payload;
+        }
     },
     extraReducers: (builder) => {
         builder
-
-            // Obtener los datos en base a los datos mas recientes que tenga
+            // Obtener los datos con paginacion
             .addCase(fetchNeows.pending, (state) => {
-                state.fetchData = { loading: true, error: null, data: null };
+                state.fetchData = { loading: true, error: null, data: state.fetchData.data ?? [] };
             })
             .addCase(fetchNeows.fulfilled, (state, action) => {
-                state.fetchData = { loading: false, error: null, data: action.payload };
+                const newData = action.payload ?? [];
+                const existingData = state.fetchData.data ?? [];
+
+
+                // Filtrar los duplicados, manteniendo solo elementos que no existan en el estado
+                const mergedData = [...existingData, ...newData].filter((value, index, self) =>
+                    index === self.findIndex((t) => (
+                        t.id === value.id
+                    ))
+                );
+
+                // Agregar los nuevos datos al final de los existentes
+                state.fetchData = {
+                    loading: false,
+                    error: null,
+                    data: mergedData,
+                };
+
+                // Si no hay mas datos, marcar que no hay mas paginas
+                if (newData.length === 0 || newData.length < 20) {
+                    state.noMorePages = true;
+                } else {
+                    // Incrementamos la pagina si hay mas datos
+                    state.page += 1;
+                }
             })
             .addCase(fetchNeows.rejected, (state, action) => {
-                state.fetchData = { loading: false, error: action.error.message ?? 'Error fetching info NeoWs', data: null };
+                state.fetchData = { loading: false, error: action.error.message ?? 'Error fetching info NeoWs', data: [] };
             })
 
-            // Obtener datos en base a una fecha pasada
+            // Obtener datos de un unico NeoWs
             .addCase(fetchNeowsForId.pending, (state) => {
                 state.fetchDataForId = { loading: true, error: null, data: null };
             })
@@ -54,7 +91,7 @@ export const neowsSlice = createSlice({
                 state.fetchDataForId = { loading: false, error: action.error.message ?? 'Error fetching id selected info NeoWs', data: null };
             })
 
-            // Obtener los datos guardados
+            // Obtener datos offline
             .addCase(fetchNeowsOffline.pending, (state) => {
                 state.fetchDataOffline = { loading: true, error: null, data: null };
             })
@@ -65,17 +102,16 @@ export const neowsSlice = createSlice({
                 state.fetchDataOffline = { loading: false, error: action.error.message ?? 'Error fetching NeoWs offline', data: null };
             })
 
-            // Guardar los datos
+            // Guardar datos
             .addCase(saveNeows.fulfilled, (_state, _action) => {
             })
 
-            // Eliminar los datos
+            // Eliminar datos
             .addCase(deleteNeows.fulfilled, (state) => {
                 state.fetchDataOffline = { loading: false, error: null, data: null };
             });
     },
 });
 
-
-export const { clearNeows } = neowsSlice.actions;
+export const { clearNeows, setNoMorePages, setPage } = neowsSlice.actions;
 export default neowsSlice.reducer;
