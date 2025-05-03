ba519f662ab49b8aa75676464f8182cd91c18ef1
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-05-03 08:11:19 -0600

    Update RootNavigator.tsx

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -4,6 +4,7 @@ import HomeScreen from "@/presentation/screens/HomeScreen";
 import TabsStack from "./stacks/TabsStack";
 import DrawerStack from "./stacks/DrawerStack";
 import SearchImageScreen from '@/presentation/screens/earth/SearchImageScreen';
+import NeowsScreen from '@/presentation/screens/neows/NeowsScreen';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -14,6 +15,7 @@ export default function RootNavigator() {
             <Stack.Screen name="Apod" component={TabsStack} options={{ title: 'Explorando APOD' }} />
             <Stack.Screen name="Earth" component={SearchImageScreen} options={{ title: 'Explorando Earth' }} />
             <Stack.Screen name="Epic" component={DrawerStack} options={{ headerShown: false }} />
+            <Stack.Screen name="Neows" component={NeowsScreen} options={{ title: 'Explorando NeoWs' }} />
         </Stack.Navigator>
     );
 }
