0cf1e9887768da91093ed0c30a4e3c3d4fe5562b
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-05-03 08:18:55 -0600

    Update useNeowsViewModel.ts
    Cambios del manejo de estado conectando lo creado en el slice y haciendo uso de los datos dentro de este y previniendo ejecutar varias veces una peticion mientras se encuentra cargando

--- src/presentation/viewmodels/useNeowsViewModel.ts
+++ src/presentation/viewmodels/useNeowsViewModel.ts
@@ -2,29 +2,37 @@ import { useCallback, useState } from "react";
 import { useAppDispatch, useAppSelector } from "../hooks/useContextHook";
 import { deleteNeows, fetchNeows, fetchNeowsForId, saveNeows } from "@/store/thunks/neowsThunk";
 import { NeoWs } from "@/domain/entities/NeoWs";
-import { clearNeows } from "@/store/slices/neowsSlice";
+import { clearNeows, setPage, setNoMorePages } from "@/store/slices/neowsSlice";
 
 export const useNeowsViewModel = () => {
     const dispatch = useAppDispatch();
-
-    // State para controlar la pagina
-    const [page, setPage] = useState<number>(0);
 
     // Acceder al estado de neows
     const neowsData = useAppSelector(state => state.neows.fetchData);
     const neowsDataForId = useAppSelector(state => state.neows.fetchDataForId);
     const offilineNeows = useAppSelector(state => state.neows.fetchDataOffline);
+    const page = useAppSelector(state => state.neows.page);
+    const noMorePages = useAppSelector(state => state.neows.noMorePages);
 
-    // Acciones encapsuldas
+    // Estado para controlar si estamos cargando mas datos
+    const [isLoadingMore, setIsLoadingMore] = useState<boolean>(false);
+
+    // Funcion para cargar los datos
     const fetchDataNeows = useCallback(() => {
-        dispatch(fetchNeows(page));
-        setPage(antPage => antPage + 1);
-    }, [dispatch]);
+        if (isLoadingMore || noMorePages) return;
+    
+        setIsLoadingMore(true);
+        dispatch(fetchNeows(page)).then(() => {
+            setIsLoadingMore(false);
+        });
+    }, [dispatch, isLoadingMore, noMorePages, page]);
 
+    // Función para cargar datos de un unico NeoWs
     const fetchDataForId = useCallback((id: string) => {
         dispatch(fetchNeowsForId(id));
     }, [dispatch]);
 
+    // Funcion para guardar un NeoWs
     const saveCurrentNeows = useCallback((data: NeoWs | null) => {
         if (!data) {
             console.log('No tuve datos, no ejecute la accion de guardado');
@@ -33,13 +41,26 @@ export const useNeowsViewModel = () => {
         dispatch(saveNeows(data));
     }, [dispatch]);
 
+    // Funcion para eliminar los datos guardados
     const deleteSavedDataNeows = useCallback(() => {
         dispatch(deleteNeows());
     }, [dispatch]);
 
+    // Funcion para limpiar el estado de Neows
     const clearNeowsState = useCallback(() => {
         dispatch(clearNeows());
     }, [dispatch]);
+
+    // Funcion para manejar la página y los datos cargados
+    const handlePagination = useCallback(() => {
+        if (neowsData.data && neowsData.data.length === 0) {
+            // Si no hay mas datos, marcar que no hay mas páginas
+            dispatch(setNoMorePages(true));
+        } else {
+            // Si hay mas datos, actualizar la pagina
+            dispatch(setPage(page + 1));
+        }
+    }, [dispatch, neowsData.data, page]);
 
     return {
         neowsData,
@@ -49,7 +70,9 @@ export const useNeowsViewModel = () => {
         fetchDataForId,
         saveCurrentNeows,
         clearNeowsState,
-        deleteSavedDataNeows
+        deleteSavedDataNeows,
+        handlePagination,
+        isLoadingMore,
+        noMorePages
     };
-
 };
