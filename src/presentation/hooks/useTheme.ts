28e83f09164886075fdea0a91c02e86c72d41ee7
David Trejo <149961434+D4T4-DEV@users.noreply.github.com>
2025-05-09 13:29:53 -0600

    Update useTheme.ts
    Guardado del tema y carga del tema cuando se cmabia

--- src/presentation/hooks/useTheme.ts
+++ src/presentation/hooks/useTheme.ts
@@ -2,11 +2,15 @@ import { darkTheme, lightTheme } from "@/core/themes";
 import { useAppDispatch, useAppSelector } from "./useContextHook"
 import { DarkTheme as navDark, DefaultTheme as navLight } from "@react-navigation/native";
 import { setDarkMode, setThemeBasedTime } from "@/store/slices/themeSlice";
+import { KeysStorage_Configuration } from "@/core/keys_storage";
 import { useEffect, useState } from "react";
+import { LocalStorage } from "@/services/storage";
 
 export const useTheme = () => {
     const dispatch = useAppDispatch();
     const isDarkContext = useAppSelector((state) => state.theme.isDarkMode);
+
+    const THEME_KEY = KeysStorage_Configuration.THEME_PERSISTENCE_KEY;
 
     // useStates
     const [hour, setHour] = useState<number | undefined>(8);
@@ -16,15 +20,26 @@ export const useTheme = () => {
     const navigationTheme = isDarkContext ? navDark : navLight // tema para react-navigation
 
     // handlers
-    const setTheme = (isDarkMode: boolean) => {
+    const setTheme = async (isDarkMode: boolean) => {
         setHour(_ => isDarkMode ? 22 : 0);
-        dispatch(setDarkMode(isDarkMode))
+        dispatch(setDarkMode(isDarkMode));
     }
 
-    const setThemeBasedHour = (cumstomHour?: number) => {
+    const setThemeBasedHour = async (cumstomHour?: number) => {
         const hour = cumstomHour ?? new Date().getHours();
         setHour(hour);
         dispatch(setThemeBasedTime(hour));
+    }
+
+    const loadTheme = async () => {
+        const dataTheme = await LocalStorage.load<boolean>(THEME_KEY);
+        if (typeof dataTheme === "boolean") {
+            dispatch(setDarkMode(dataTheme));
+        }
+    };
+
+    const savedTheme = async (isDarkMode: boolean) => {
+        await LocalStorage.save<boolean>(THEME_KEY, isDarkMode);
     }
 
     // // useEffects
@@ -35,6 +50,16 @@ export const useTheme = () => {
     // }, [hour]);
     // Comentamos esta linea esto era para cuando inicie la aplicación 
     // setee el tema en base el horario del dispositivo
+
+    // Carga los datos guardados del tema
+    useEffect(() => {
+        loadTheme();
+    }, []);
+
+    // Guarda el tema siempre que cambia su estado
+    useEffect(() => {
+        savedTheme(isDarkContext);
+    }, [isDarkContext]);
 
     return {
         isDarkContext,
